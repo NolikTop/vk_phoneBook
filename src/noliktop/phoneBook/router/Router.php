92abc6c859c701f632fd3fe1621ae5976462332d
NolikTop <aydansuper12@gmail.com>
2022-04-04 03:57:46 +0300

    feat(Router): часть, отвечающая за определение нужного контроллера и метода по url

---
+++ src/noliktop/phoneBook/router/Router.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace noliktop\phoneBook\router;
+
+
+use noliktop\phoneBook\controller\ControllerException;
+use noliktop\phoneBook\controller\ControllerFactory;
+use noliktop\phoneBook\exception\ErrorCodes;
+use noliktop\phoneBook\exception\HTTPCodes;
+
+abstract class Router {
+
+	protected ControllerFactory $controllerFactory;
+
+	public function __construct(ControllerFactory $controllerFactory){
+		$this->controllerFactory = $controllerFactory;
+	}
+
+	/**
+	 * @throws RouterException
+	 * @throws ControllerException
+	 */
+	public function getCallableMethod(string $path): callable{
+		[$controllerName, $methodName] = $this->getControllerAndMethodNameFromPath($path);
+		$controller = $this->controllerFactory->getController($controllerName);
+
+		$callable = [$controller, $methodName];
+		if(!is_callable($callable)){
+			throw new RouterException("No method $controllerName.$methodName", HTTPCodes::NOT_FOUND, ErrorCodes::CONTROLLER_NOT_FOUND);
+		}
+
+		return $callable;
+	}
+
+	abstract public function getControllerAndMethodNameFromPath(string $path): array;
+
+}
---
+++ src/noliktop/phoneBook/router/RouterException.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace noliktop\phoneBook\router;
+
+
+use noliktop\phoneBook\exception\AppException;
+
+class RouterException extends AppException {
+
+}
