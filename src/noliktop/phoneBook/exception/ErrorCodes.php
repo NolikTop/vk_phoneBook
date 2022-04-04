88cb108c64c26ccd69f773f771bdd73fa28296df
NolikTop <aydansuper12@gmail.com>
2022-04-04 03:23:25 +0300

    feat: константы для http кодов и кодов ошибок

---
+++ src/noliktop/phoneBook/exception/ErrorCodes.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace noliktop\phoneBook\exception;
+
+
+interface ErrorCodes {
+
+	const WRONG_TYPE = 1;
+	const WRONG_TOKEN = 2;
+	const NO_VALUE_FOR_PARAMETER = 3;
+	const VALUE_IS_TOO_LONG = 4;
+	const VALUE_MUST_BE_NOT_NEGATIVE_INTEGER = 5;
+	const CONTROLLER_NOT_FOUND = 6;
+	const METHOD_NOT_FOUND = 7;
+
+	const REVIEW_HAS_BEEN_DELETED_OR_DOESNT_EXIST = 20;
+
+}
